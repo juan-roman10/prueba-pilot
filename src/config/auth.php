49aeeb42c2e0542402bc289000f679d24ab673ec
Mariano Paduani <gaston.paduani15@gmail.com>
2026-08-26 11:28:43 -0300

    feat: configure JWT authentication guard

--- src/config/auth.php
+++ src/config/auth.php
@@ -16,8 +16,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'guard' => 'api',
+        'passwords' => 'users',
     ],
 
     /*
@@ -42,6 +42,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+        'driver' => 'jwt',
+        'provider' => 'users',
+    ],
     ],
 
     /*
