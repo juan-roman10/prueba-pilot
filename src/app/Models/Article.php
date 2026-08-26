fb3fab52492c911ffb95279ad72eaad6dbe14dd4
Mariano Paduani <gaston.paduani15@gmail.com>
2026-08-26 13:03:16 -0300

    feat: add Article model and establish many-to-many relationship with Category

---
+++ src/app/Models/Article.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'titulo',
+        'slug',
+        'contenido',
+        'estado',
+        'fecha_publicacion',
+    ];
+
+    protected $casts = [
+        'fecha_publicacion' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'article_category');
+    }
+}
